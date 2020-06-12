368bc2ccf66afc16b99e31855e860bcbb268beeb
Marc Grimpo <mgrimpo.dev@gmail.com>
2020-06-12 23:36:54 +0200

    Add context menu entry

--- background_script.js
+++ background_script.js
@@ -14,3 +14,16 @@ async function copySelectedTabs(event) {
 }
 
 browser.browserAction.onClicked.addListener(copySelectedTabs);
+
+browser.contextMenus.create({
+  id: "copy-selected-tab-info",
+  //title: browser.i18n.getMessage("contextMenuItemSelectionLogger"),
+  title: "Copy title and url of selected tabs to clipboard",
+  contexts: ["tab"],
+});
+
+browser.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "copy-selected-tab-info") {
+    copySelectedTabs();
+  }
+});
