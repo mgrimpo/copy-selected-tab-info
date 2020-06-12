4d67eafdb1c967f5355a9cd5a3cdc3897cb0cd9c
Marc Grimpo <marc.grimpo@posteo.de>
2020-06-12 23:20:00 +0200

    Add minimal working implementation

--- background_script.js
+++ background_script.js
@@ -1,1 +1,16 @@
-// Put all the javascript code here, that you want to execute in background.
+async function getSelectedTabs() {
+  return browser.tabs.query({
+    highlighted: true,
+  });
+}
+
+function tabsToText(tabs) {
+  return tabs.map((tab) => `${tab.title}, ${tab.url}`).join("\n");
+}
+
+async function copySelectedTabs(event) {
+  const tabsAsText = tabsToText(await getSelectedTabs());
+  navigator.clipboard.writeText(tabsAsText);
+}
+
+browser.browserAction.onClicked.addListener(copySelectedTabs);
